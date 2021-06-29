0cac28ff516144f339c09fa84b37bd846ad63f7e
Matheus Decleve <me@matheus.in>
2021-06-28 22:54:54 -0300

    Enter in a room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { useHistory } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
 
+import { database } from '../services/firebase';
 import { useAuth } from '../hooks/useAuth';
 import { Button } from '../components/Button';
 
@@ -13,12 +15,31 @@ export function Home() {
   const history = useHistory();
   const { user, signInWithGoogle } = useAuth();
 
+  const [roomCode, setRoomCode] = useState('');
+
   async function handleCreateRoom(){
     if(!user) {
       await signInWithGoogle()
     }
 
     history.push('/rooms/new')
+  }
+
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if(roomCode.trim() === ''){
+      return;
+    }
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get()
+
+    if(!roomRef.exists()) {
+      alert('Essa sala não existe.')
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`)
   }
   
   return (
@@ -36,10 +57,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={event => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
